211d22ad525f73eac8489f8dd2eb148d1fa93721
ghuynh <gael.huynh@gmail.com>
2024-11-13 22:18:27 +0100

    Add argument parser and slightly improve code structure

--- Pyrough.py
+++ Pyrough.py
@@ -9,40 +9,64 @@
 # this the code will read the parameters from the json file provided and return an stl if FEM is
 # desired or lmp file if MD is desired.
 # ---------------------------------------------------------------------------
-import os
+import argparse
 import subprocess
-import sys
+from pathlib import Path
 
-from src import Param_class, Sample_class
+from src.Param_class import Parameter
+from src.Sample_class import Sample, make_atom_grain, make_box
 
-print("#######################################################################################")
-print("#                                       Pyrough                                       #")
-print("#                          Jonathan Amodeo & Hugo Iteney 2023                         #")
-print("#######################################################################################")
 
-# _____________________Main Code____________________
-if sys.argv[1] == "-surface":
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    subprocess.call(
-        [
-            "python",
-            current_dir + "/src/Surface_Analysis.py",
-            sys.argv[2],
-            sys.argv[3],
-            sys.argv[4],
-            sys.argv[5],
-        ]
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Pyrough: Generate FEM or MD files with random surface roughness."
     )
-else:
-    Param_file = sys.argv[1]
-    out_pre = os.path.basename(Param_file)[:-5]
+    parser.add_argument(
+        "config",
+        nargs="?",
+        help="Path to the json configuration file. Not required if -surface is provided.",
+    )
+    parser.add_argument(
+        "-surface",
+        nargs=4,
+        metavar=("path", "size", "zmin", "zmax"),
+        help="Surface analysis arguments.",
+    )
+    args = parser.parse_args()
 
-    param = Param_class.Parameter(Param_file)
+    if not args.config and not args.surface:
+        parser.error("Either config or -surface arguments must be provided.")
 
-    # -----------------------second task calling the nanowire into the sample to get an stl
-    # SAMPLE IS THE PATH GUIDE TO CALLING THE CLASS TO CREATE THE STL FILE
+    if args.config and args.surface:
+        parser.error("Only one of config or -surface can be provided.")
+
+    return args
+
+
+def main() -> None:
+    print("#####################################################################################")
+    print("#                                      Pyrough                                      #")
+    print("#                         Jonathan Amodeo & Hugo Iteney 2023                        #")
+    print("#####################################################################################")
+
+    args = parse_arguments()
+
+    out_pre = Path(args.config).stem
+    param = Parameter(args.config)
+
+    if args.surface:
+        current_dir = Path(__file__).resolve().parent
+        exe_path = current_dir / "src/Surface_Analysis.py"
+
+        if not exe_path.exists():
+            print(f"Error: {exe_path} not found.")
+            return
+
+        subprocess.call(["python", exe_path, *args.surface])
+        return
+
     if param.type_S == "grain":
-        vertices, FEM_stl = Sample_class.make_box(
+        vertices, FEM_stl = make_box(
             param.type_S,
             2 * (1 + param.eta),
             param.C1,
@@ -59,7 +83,7 @@ else:
             param.ext_fem,
         )
 
-        Sample_class.make_atom_grain(
+        make_atom_grain(
             FEM_stl,
             param.lattice_structure1,
             param.lattice_parameter1,
@@ -77,64 +101,64 @@ else:
             out_pre,
             param.ext_ato,
         )
-        # call make it md to create atomsk file
         print("JOB DONE!" + "  File name: " + out_pre + ".lmp")
+        return
 
-    else:
-        # THIS CLASS CALLING REQUIRES A SAMPLE WITH A DESIRED OBJECT NAME AS AN INPUT
-        sample = Sample_class.Sample(param.type_S)
+    sample = Sample(param.type_S)
 
-        vertices, FEM_stl = sample.make_stl(
-            param.type_S,
-            param.eta,
-            param.C1,
-            param.RMS,
-            param.N,
-            param.M,
-            param.radius,
-            param.length,
-            param.height,
-            param.width,
-            param.ns,
-            param.alpha,
-            param.raw_stl,
-            param.nfaces,
-            param.surfaces,
-            param.energies,
-            param.n_at,
+    vertices, FEM_stl = sample.make_stl(
+        param.type_S,
+        param.eta,
+        param.C1,
+        param.RMS,
+        param.N,
+        param.M,
+        param.radius,
+        param.length,
+        param.height,
+        param.width,
+        param.ns,
+        param.alpha,
+        param.raw_stl,
+        param.nfaces,
+        param.surfaces,
+        param.energies,
+        param.n_at,
+        param.lattice_structure,
+        param.lattice_parameter,
+        param.material,
+        param.orien_x,
+        param.orien_z,
+        out_pre,
+        param.ext_fem,
+    )
+    print("====== > FEM JOB DONE !")
+
+    for ext in param.ext_fem:
+        print("====== > File name: " + out_pre + "." + str(ext))
+
+    if "stl" not in param.ext_fem:
+        print("====== > File name: " + out_pre + ".stl")
+
+    if param.output(args.config) == "ATOM":
+        sample.make_atom(
+            FEM_stl,
             param.lattice_structure,
             param.lattice_parameter,
             param.material,
             param.orien_x,
+            param.orien_y,
             param.orien_z,
+            vertices,
             out_pre,
-            param.ext_fem,
+            param.ext_ato,
         )
+        print("====== > ATOM JOB DONE !")
 
-        print("====== > FEM JOB DONE !")
-        for ext in param.ext_fem:
+        # Print the name of the files generated
+        for ext in param.ext_ato:
             print("====== > File name: " + out_pre + "." + str(ext))
 
-        if "stl" not in param.ext_fem:
-            print("====== > File name: " + out_pre + ".stl")
 
-        # Calling the sample class function of MAKE it which returns an stl file
-        # of the object desired.
-        if param.output(Param_file) == "ATOM":
-            sample.make_atom(
-                FEM_stl,
-                param.lattice_structure,
-                param.lattice_parameter,
-                param.material,
-                param.orien_x,
-                param.orien_y,
-                param.orien_z,
-                vertices,
-                out_pre,
-                param.ext_ato,
-            )
-
-            # call make it md to create atomsk file
-            print("====== > ATOM JOB DONE !")
-            for ext in param.ext_ato:
-                print("====== > File name: " + out_pre + "." + str(ext))
+if __name__ == "__main__":
+    main()
